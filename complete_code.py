fa7207a31341152f442b30664b5b85b98a0cb2ff
ABHAY9927 <abhay5872455@gmail.com>
2025-02-05 11:33:20 +0530

    Update complete_code.py
    use documentation

--- complete_code.py
+++ complete_code.py
@@ -1,7 +1,17 @@
-# coding: utf-8
+"""This code based on python mysql 
+    we create school_management database and create three tables 
+    1st table == student(name, age, grade, email)
+    2nd table == teacher(name, address, age, email, class)
+    3rd table == class_name(subject, class, teacher_name(foreign key))
+insert the data from csv files 
+Programmer = Abhay kumar
+Date = 01/02/2025
+"""
+
 import mysql.connector
 import csv
 def insert_data_from_csv(file_name, insert_query, conn):
+    """This is the funtion to insert the data from csv files"""
     with open(file_name, "r") as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
@@ -10,7 +20,7 @@ def insert_data_from_csv(file_name, insert_query, conn):
             cursor.execute(insert_query, row)
             conn.commit()
             
-conn =mysql.connector.connect(host="localhost", port=3306, user="abhay", password="abhay",database="school_management")
+conn =mysql.connector.connect(host="localhost", port=3306, user="abhay", password="abhay",database="school_management") 
 insert_students_query = "INSERT INTO student(name, age, grade, email) VALUES (%s, %s, %s, %s)"
 insert_teachers_query = "INSERT INTO teacher(name, age, address, email, class) VALUES (%s, %s, %s, %s, %s)"
 insert_classes_query = "INSERT INTO class_name(subject, class, teacher_name) VALUES (%s, %s, %s)"
